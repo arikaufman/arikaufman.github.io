fd4fb306d073f9ccc0afa7aff2a93d50ad51242d
arikaufman <42683887+arikaufman@users.noreply.github.com>
2023-01-11 20:54:30 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,23 @@ jQuery(document).ready(function($) {
 
 
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "assets/images/Resume.png";
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var div = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+div.onclick = function() {
+  modal.style.display = "none";
+}
